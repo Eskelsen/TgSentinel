c6f1bf6cdb0430e9e6e392d9feeb487d5220fbb1
Daniel Eskelsen <eskelsen@yahoo.com>
2026-04-01 14:23:55 -0300

    Drafts of the project scope

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+# Telegram Sentinel
+
+/* 
+ * Planejamento de escopo do projeto
+ * 
+ * Configuração do bot
+ * - Instalação do token > configuração de webhooks
+ * - Tratamento de retorno de configuração webhook
+ * 
+ * Registro de usuário
+ * - Persistência e atualização
+ * - Gerenciamento de usuário
+ * - Comandos primitivos
+*/
