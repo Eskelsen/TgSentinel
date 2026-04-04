9feb8eb71294115c827fb7c34b7b7a1c323aaf57
Daniel Eskelsen <eskelsen@yahoo.com>
2026-04-04 00:43:41 -0300

    Update webhooks stream

--- config.php
+++ config.php
@@ -8,3 +8,5 @@ error_reporting(E_ALL);
 # Erros Log Output
 ini_set('log_errors', DEV);
 ini_set('error_log', __DIR__  . '/errors.log');
+
+date_default_timezone_set(TIMEZONE);
--- index.php
+++ index.php
@@ -21,4 +21,18 @@ include __DIR__ . '/TgClient.php';
 
 $data = TgClient::treatWebhook();
 
-error_log(json_encode($data));
+// error_log(json_encode($data));
+
+if (empty($data['chat_id'])) {
+    error_log('Sem chat_id');
+}
+
+extract($data);
+
+$tg = new TgClient(TG_TOKEN);
+
+$msg = "Olá, $data! Seu sobrenome é $surname, nome de usuário $username e o seu chat_id é $chat_id";
+
+$response = $tg->sendMessage($chat_id,$msg);
+
+error_log(json_encode($response));
